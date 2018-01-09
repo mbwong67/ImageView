50f7a1d2c37efc4f01378e1448a2509bde1709b2
Richard Vogt <richvogtjr@gmail.com>
2018-01-09 17:34:23 -0600

    let's get Pikachu on the screen

---
+++ app/src/main/java/com/nastysloper/imageviewexample/TurtleActivity.java
@@ -0,0 +1,13 @@
+package com.nastysloper.imageviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TurtleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turtle);
+    }
+}
